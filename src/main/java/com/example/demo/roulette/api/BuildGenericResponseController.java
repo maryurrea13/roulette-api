9deaa8d355f4b69c536ab9c2bda691b942d73e09
maryurrea5 <maryurrea5@gmail.com>
2021-03-03 20:14:04 -0500

    cambios en builderGenericResponsecontroller

--- src/main/java/com/example/demo/roulette/api/BuildGenericResponseController.java
+++ src/main/java/com/example/demo/roulette/api/BuildGenericResponseController.java
@@ -5,22 +5,10 @@ import org.springframework.http.ResponseEntity;
 
 public abstract class BuildGenericResponseController<T> {
 
-  /*  public ResponseEntity<RestResponse<Map>> buildListResponse(String value, HttpStatus status, Map obj, String errorMsg ){
-        return new ResponseEntity<>(
-                new RestResponse<Map>(status.value(), errorMsg, value , obj),
-                HttpStatus.valueOf(status.name()));
-    }*/
-
     public ResponseEntity<RestResponse<T>> buildResponse(String message,  T obj){
         return new ResponseEntity<>(
                 new RestResponse<>(message, obj), HttpStatus.OK);
     }
 
-  /*  public ResponseEntity<RestResponse<T>> buildResponse(String message, HttpStatus status){
-        return buildResponse(message, status, null, null);
-    }
 
-    public ResponseEntity<RestResponse<T>> buildErrorResponse(String value, HttpStatus status){
-        return buildResponse(null, status, null, value);
-    }*/
 }
